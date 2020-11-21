53b45eb500d887b5da3e9775ca969f76d055a8cb
eniddealla <allaeddinekhalid@gmail.com>
2020-11-21 19:24:03 +0100

    some stuff

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,10 +6,44 @@
 /*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 16:18:51 by akhalid           #+#    #+#             */
-/*   Updated: 2020/11/21 16:33:22 by akhalid          ###   ########.fr       */
+/*   Updated: 2020/11/21 17:07:38 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#IFNDEF 1
+#ifndef MINIRT_H
+# define MINIRT_H
 
-#
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <mlx.h>
+
+typedef struct  s_scene
+{
+    
+}               t_scene;
+
+
+typedef	struct	s_image {
+	void		*img;
+	int			*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_image;
+
+typedef struct  s_options
+{
+    t_scene *scene;
+    void    *mlx;
+    void    *window;
+    t_image image;
+}               t_options;
+
+
+void minirt(int fd, int save);
+
+
+#endif
--- srcs/minirt.c
+++ srcs/minirt.c
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minirt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/21 16:13:07 by akhalid           #+#    #+#             */
-/*   Updated: 2020/11/21 16:54:41 by akhalid          ###   ########.fr       */
+/*   Created: 2020/11/21 16:54:52 by akhalid           #+#    #+#             */
+/*   Updated: 2020/11/21 17:01:35 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minirt.h"
 
-int main(int argc, char *argv[])
+void minirt(int fd, int save)
 {
-    int fd;
     
-    
-    if (argc < 2 || argc > 3)
-        return (0);
-    else
-    {
-        fd = open(argv[1], O_RDONLY);
-        if (argc == 2)
-            minirt(fd, 0);
-        else
-            minirt(fd, 1);
-    }
-    
-    return (0);
 }
